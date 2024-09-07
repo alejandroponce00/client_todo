2abbcba13b9f8c08514f8e777bdde4908d7089eb
Alejandro Ponce <122680586+alejandroponce00@users.noreply.github.com>
2024-09-07 20:25:00 -0300

    conexion con django en railway

--- src/app/components/TodoCRUD.tsx
+++ src/app/components/TodoCRUD.tsx
@@ -16,7 +16,7 @@ interface Todo {
   completed: boolean;
 }
 
-const API_BASE_URL =`${process.env.NEXT_PUBLIC_BACKEND_URL}`
+const API_BASE_URL =`${process.env.NEXT_PUBLIC_BACKEND_URL}/api`
 
 export default function Component() {
   const [todos, setTodos] = useState<Todo[]>([])
