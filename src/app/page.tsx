ef65f0cbced660af3086cd4bbbcc6ecae2755d93
Alejandro Ponce <122680586+alejandroponce00@users.noreply.github.com>
2024-09-07 14:16:37 -0300

    footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ export default function Home() {
     <div className="min-h-screen bg-background">
       <h1 className="text-4xl font-bold text-center mb-8">Todo App</h1>
       <Component />
+      <footer className='text-center mt-20'>Creado por AlejandroPonce</footer>
     </div>
   )
 }
